bd537c655cbc8fe135e7fc33c3245904f54aaf2e
ruiqing.zhang18@gmail.com <ruiqing.zhang18@gmail.com>
2018-03-10 23:45:09 -0500

    Created foundation design

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import classes from './App.css';
+
+class App extends Component {
+  state = {
+    
+  }
+
+  render () {
+
+    return (
+      
+      <div> Hello </div>
+    );
+  }
+}
+
+export default App;
