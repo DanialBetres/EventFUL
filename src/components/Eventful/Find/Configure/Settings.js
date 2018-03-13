f1e25220fc7eed1c5db3879aad1a0840376ce8b3
ruiqing.zhang18@gmail.com <ruiqing.zhang18@gmail.com>
2018-03-13 01:08:24 -0400

    updated view and settings

--- src/components/Eventful/Find/Configure/Settings.js
+++ src/components/Eventful/Find/Configure/Settings.js
@@ -2,19 +2,50 @@ import React, {Component} from 'react';
 import classes from './Settings.css';
 import Aux from '../../../../hoc/AuxA';
 import DatePicker from 'material-ui/DatePicker';
-
-
-
+import Button from '../../../UI/Buttons/Button';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
+import ActionFavorite from 'material-ui/svg-icons/action/favorite';
+import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
 
 class Settings extends Component{
   state = {
-
+    dropDownValue : 1,
+    radioButtonValue: 1,
   }
+  handleChange = (event, index, dropDownValue) => this.setState({dropDownValue});
 
   render(){
     return(
       <div>
+        <RadioButtonGroup name="shipSpeed" defaultSelected="not_light">
+          <RadioButton
+            value={1}
+            label="< 1 km"
+            style={classes.radioButton}
+          />
+          <RadioButton
+            value={2}
+            label="< 5 km"
+            style={classes.radioButton}
+          />
+          <RadioButton
+            value={3}
+            label="5km +"
+            style={classes.radioButton}
+          />
+        </RadioButtonGroup>
+        <DropDownMenu value={this.state.dropDownValue} onChange={this.handleChange}>
+          <MenuItem value={1} primaryText="Eg. Eat, Exercise etc." />
+          <MenuItem value={2} primaryText="Eat" />
+          <MenuItem value={3} primaryText="Exercise" />
+          <MenuItem value={4} primaryText="Swim" />
+          <MenuItem value={5} primaryText="Entertainment" />
+        </DropDownMenu>
+        
         <DatePicker hintText="Choose a Date" />
+        <Button> Find </Button>
       </div> 
     );
     // return(
--- src/components/Eventful/View/View.js
+++ src/components/Eventful/View/View.js
@@ -1,52 +1,108 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom'
 import classes from './View.css';
 import {GridList, GridTile} from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
 import Subheader from 'material-ui/Subheader';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 import axios from 'axios';
+import SearchBar from 'material-ui-search-bar';
+import SearchInput, {createFilter} from 'react-search-input'
+
+const KEYS_TO_FILTERS = ['TITLE', 'CATEGORY']
 
 
 class View extends Component {
   state = {
-    events:[]
+    events:[],
+    searchTerm : '',
+    currentPage: 1,
+    todosPerPage: 6
+
   };
+  constructor (props){
+    super(props)
+    this.searchUpdated = this.searchUpdated.bind(this);
+    // this.handleClick = this.handleClick.bind(this);
+  }
+  // handleClick(event) {
+  //       this.setState({
+  //         currentPage: Number(event.target.id)
+  //       });
+  // }
 
   componentDidMount () {
       // console.log( this.props );
       axios.get( '/events.json' )
           .then( response => {
             let rawEvents = response.data;
-
             this.setState({events: rawEvents});
-
-              console.log( response.data[0] );
+            // console.log( response.data[0] );
           } )
           .catch( error => {
               console.log( error );
           } );
   }
+  searchUpdated (term) {
+    this.setState({searchTerm: term})
+  }
 
   render() {
-    const events2 = this.state.events.map((res, i) => (
-      <GridTile
-        key={res.ID  + i}
-        title={res.TITLE}
-        subtitle={<span>Category: <b>{res.CATEGORY}</b></span>}>
-        <img src={res.VIDEO} />
-      </GridTile>
-    ));
+    // const { events, currentPage, todosPerPage } = this.state;
+    // // Logic for displaying current todos
+    // const indexOfLastTodo = currentPage * todosPerPage;
+    // const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
+    // const currentTodos = events.slice(indexOfFirstTodo, indexOfLastTodo);
+    // const renderTodos = currentTodos.map((events, index) => {
+    //   return <li key={index}>{events}</li>;
+    // });
+
+    // Logic for displaying page numbers
+    // const pageNumbers = [];
+    // for (let i = 1; i <= Math.ceil(events.length / todosPerPage); i++) {
+    //   pageNumbers.push(i);
+    // }
+    // const renderPageNumbers = pageNumbers.map(number => {
+    //   return (
+    //     <li
+    //       key={number}
+    //       id={number}
+    //       onClick={this.handleClick}
+    //     >
+    //       {number}
+    //     </li>
+    //   );
+    // });
+    const filteredEvents = this.state.events.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
+
+    let searchResults = filteredEvents.map((res,i) => {
+          return (
+            <GridTile
+              key={res.ID  + i}
+              title={res.TITLE}
+              subtitle={<span>Category: <b>{res.CATEGORY}</b></span>}>
+              <img src={res.VIDEO} />
+            </GridTile>
+          )
+    });
+
     return (
       <div>
-      <div className={classes.root}>
+        <SearchBar
+         onChange={this.searchUpdated}
+         onRequestSearch={() => console.log('onRequestSearch')}
+         style={{
+           margin: '0 auto',
+           maxWidth: 800
+         }}
+        />
+        <div className={classes.root}>
         <GridList
           cellHeight={180}
           className={classes.gridList}>
-          <Subheader>View</Subheader>
-          {events2}
+          {searchResults}
         </GridList>
-
-      </div>
+        </div>
 
       </div>
     )
