d64afa6ce1657abecfb4c4b9a078a777ddcdefaf
Danial Betres <danialbet@gmail.com>
2018-03-13 12:52:49 -0400

    changes

---
+++ src/components/Eventful/login/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from '../../UI/Buttons/Button';
+
+const login = (props) => {
+
+    return (
+        <div>
+            <Button>Login</Button>
+            <Button>Sign Up</Button>
+
+        </div>
+    )
+
+}
+
+export default login;
